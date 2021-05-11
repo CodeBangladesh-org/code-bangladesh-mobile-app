412c02062e35afc43ad5e0947f44a16b2a26dcfc
Mossaddeque Mahmood <mmahmood.ict.bd@gmail.com>
2021-05-11 14:21:21 +0200

    Add comments on the code.

--- lib/dto/category_stat_response_dto.dart
+++ lib/dto/category_stat_response_dto.dart
@@ -5,6 +5,8 @@ class CategoryStatResponseDTO {
 
   CategoryStatResponseDTO({this.numberOfCourses, this.numberOfVideos, this.numberOfTopics});
 
+  // On app-data.json, number_of_courses, number_of_videos and number_of_topics for category is in Bangla numerals.
+  // No need to convert.
   factory CategoryStatResponseDTO.fromJson(Map<String, dynamic> json) {
     return CategoryStatResponseDTO(
       numberOfCourses: json['number_of_courses'] as String,
